1c8aa7ed2605ee165fbbc29e94aeb50a5af2680e
loc131199 <108286623+loc131199@users.noreply.github.com>
2025-06-24 22:24:54 +0700

    Update config.py
    thay đổi kết nối neo4j

--- config.py
+++ config.py
@@ -1,13 +1,11 @@
-# config.py
-
 import os
 
 # --- Cấu hình Neo4j AuraDB Kết nối Neo4j với đồ thị---
-NEO4J_URI = "neo4j+s://2647eb1b.databases.neo4j.io"
+NEO4J_URI = "neo4j+s://289c66da.databases.neo4j.io"
 NEO4J_USERNAME = "neo4j"
-NEO4J_PASSWORD = "5pkhR9fSPDSnN1sDr04zHSVLj7KKl0xhsQn81OcTnVg"
+NEO4J_PASSWORD = "xFi091FXCqQ4lSpgvTIvULah4S-UJwagPZYkQYRqmQA"
 
-GEMINI_API_KEY_DIRECT = "AIzaSyDVL_GyA-T6FlQQs4UvZRlNrJh8xOwH89E"
+GEMINI_API_KEY_DIRECT = "AIzaSyB_nhTahlsc3MapsSrXep9U-117KRTNLgI"
 
 MODEL_EMBEDDING = "models/embedding-001"
-MODEL_LLM = "gemini-pro"
+MODEL_LLM = "models/gemini-1.5-pro-latest"
